6e3a3d9a515292dcc32515b24fadb07ec875f6a6
Aditya Singh <122855095+adityasingh151@users.noreply.github.com>
2023-05-12 09:39:00 +0530

    Update Record.java

--- OopsProject2/Record.java
+++ OopsProject2/Record.java
@@ -7,84 +7,84 @@ public class Record {
     private int idNumber;
     private long contactNumber;
 
-    /**
-     * Default Constructor.
-     */
+    
+     // Default Constructor.
+     
     public Record() {
     }
 
-    /**
-     * Parameterized Constructor.
-     *
-     *  name
-     *  idNumber
-     *  contactNumber
-     */
+    
+     // Parameterized Constructor.
+     
+     //  name
+     //  idNumber
+     //  contactNumber
+     
     public Record(String name, int idNumber, long contactNumber) {
         this.name = name;
         this.idNumber = idNumber;
         this.contactNumber = contactNumber;
     }
 
-    /**
-     * Get the value of contactNumber
-     *
-     * @return the value of contactNumber
-     */
+    
+     // Get the value of contactNumber
+     
+     // return the value of contactNumber
+     
     public long getContactNumber() {
         return contactNumber;
     }
 
-    /**
-     * Set the value of contactNumber
-     *
-     *  contactNumber new value of contactNumber
-     */
+    
+     // Set the value of contactNumber
+    
+     //  contactNumber new value of contactNumber
+     
     public void setContactNumber(int contactNumber) {
         this.contactNumber = contactNumber;
     }
 
-    /**
-     * Get the value of idNumber
-     *
-     *  the value of idNumber
-     */
+    
+     // Get the value of idNumber
+     
+      // the value of idNumber
+     
     public int getIdNumber() {
         return idNumber;
     }
 
-    /**
-     * Set the value of idNumber
-     *
-     *  idNumber new value of idNumber
-     */
+    
+     // Set the value of idNumber
+     
+     // idNumber new value of idNumber
+     
     public void setIdNumber(int idNumber) {
         this.idNumber = idNumber;
     }
 
-    /**
-     * Get the value of name
-     *
-     *  the value of name
-     */
+    
+     // Get the value of name
+     
+     //  the value of name
+     
     public String getName() {
         return name;
     }
 
-    /**
-     * Set the value of name
-     *
-     *  name new value of name
-     */
+    
+     // Set the value of name
+     
+      // name new value of name
+     
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * toString Method.
-     *
-     * @return
-     */
+    
+     // toString Method.
+     
+    // @return
+     
     @Override
     public String toString() {
         return "Records{" + "name=" + name + ", idNumber=" + idNumber + ", contactNumber=" + contactNumber + '}';
